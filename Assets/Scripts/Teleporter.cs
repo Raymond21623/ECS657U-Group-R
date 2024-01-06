10155966cbc6ac93736f97d22171c5240b54382a
Sharusan17 <shariharan17@icloud.com>
2024-01-06 15:25:50 +0000

    Update Teleporter.cs

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -3,20 +3,16 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour
 {
     public Transform player, destination;
-    public Character character; // Reference to the Character script
+    public Character character;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            // Instead of disabling the player game object, you might disable specific components if needed.
-            // For example, disable the controller during teleportation to avoid conflicts:
             character.controller.enabled = false;
 
-            // Teleport the player
             player.position = new Vector3(destination.position.x, player.position.y, destination.position.z);
 
-            // If any components were disabled, re-enable them here:
             character.controller.enabled = true;
         }
     }
